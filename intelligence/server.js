6cf1c6c8ddb8068ab4121907b12ece7c31cc7c17
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-01-03 20:27:30 +0530

    conversations error

--- intelligence/server.js
+++ intelligence/server.js
@@ -1,9 +1,9 @@
+require('dotenv').config(); // Ensure this is at the top
 const express = require('express');
 const connectDB = require('./src/config/db');
 const chatbotRoutes = require('./src/routes/intelligenceRoutes');
 
-require('dotenv').config(); // Load environment variables
-
+// Initialize the Express app
 const app = express();
 
 // Connect to MongoDB
@@ -15,4 +15,5 @@ app.use('/api/intelligence', chatbotRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
