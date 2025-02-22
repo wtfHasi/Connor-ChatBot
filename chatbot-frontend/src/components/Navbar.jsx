b7caf876764c057c3a605b7289c964f2843f55cc
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-02-22 23:00:49 +0530

    update navbar.css

--- chatbot-frontend/src/components/Navbar.jsx
+++ chatbot-frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import "./Navbar.css";
+import "./navbar.css";
 
 const Navbar = () => {
   const [scrolling, setScrolling] = useState(false);
