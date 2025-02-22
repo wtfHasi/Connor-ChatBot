e8c7f80571d5be250598ec8e5a62f77d0d0cfde6
Hasith Sandesh Vitanage <129611058+wtfHasi@users.noreply.github.com>
2025-02-22 23:02:29 +0530

    update signup.jsx

--- chatbot-frontend/src/components/SignUp.jsx
+++ chatbot-frontend/src/components/SignUp.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { registerUser } from "../api";
-import "./Signup.css";
+import "./signup.css";
 
 const Signup = () => {
   const navigate = useNavigate();
